831e562b578474a4a47f93faca33572852abafee
Aurelie Miomandre <aureliemiomandre@hotmail.fr>
2019-10-07 16:39:28 +0200

    atelier 1 react

---
+++ src/Hello.js
@@ -0,0 +1,13 @@
+import React from 'react'; 
+import {Component} from 'react';
+
+class Hello extends Component{
+    render(){
+        return(
+            <div>
+            <h1>Hello React World</h1>
+            </div>)
+    }
+}
+
+export default Hello
