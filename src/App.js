0762e345c8fd3dbe5695ee7b92f62c9e38eecfe5
AurelieMio <aureliemiomandre@hotmail.fr>
2019-11-27 10:26:26 +0100

    quete finie

--- src/App.js
+++ src/App.js
@@ -1,37 +1,96 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+
+
+
+
+
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      working: props.false
-    };
-    this.state.working = this.state.working.bind(this)
-    this.setState = this.setState.bind(this);
+      title: "",
+      poster: '',
+      comment:'',
+    }
+    this.onChange= this.onChange.bind(this)
+    this.submitForm = this.submitForm.bind(this)
   }
+  onChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  }
+submitForm(e) {
+  e.preventDefault();
+  const url = "https://post-a-form.herokuapp.com/api/movies"
+  const config = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify(this.state),
+  };
+  fetch(url, config)
+  .then (res => res.json())
+    .then(res => {
+      if (res.error){
+        alert(res.error)
+      } else { alert(`Film ${this.state.title} ajouté à la liste`);
+    }
+    })
+    .catch(e=> {
+      console.error(e);
+      alert("Erreur pendant l'ajout du film")
+    })
+
+  }
+
+
   render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" className={this.props.working ? "workingLogo": "notWorkingLogo"} alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        </header>
-        <button
-        onClick={() => this.setState(state => ({working: !state.working }))}
-        >
-        </button>
-      
+      <form onSubmit={this.submitForm}>
+        <fieldset>
+          <div className="form-data">
+            <label htmlFor="name">Title</label>
+            <input
+              type="text"
+              id="title"
+              name="title"
+              onChange={this.onChange}
+              value={this.state.title}
+              required
+            />
+          </div>
+          <div className="form-data">
+            <label htmlFor="url">Affiche</label>
+            <input
+              type="url"
+              id="poster"
+              name="poster"
+              onChange={this.onChange}
+              value={this.state.poster}
+              required
+            />
+          </div>
+          <div className="form-data">
+            <label htmlFor="Comment">Comment</label>
+            <input
+              type="text"
+              id="comment"
+              name="comment"
+              onChange={this.onChange}
+              value={this.state.comment}
+            />
+          </div>
+          <div className="form-data">
+          <input type="submit" value="Envoyer" />
+          </div>
+        </fieldset>
+      </form>
     </div>
   );}
 }
